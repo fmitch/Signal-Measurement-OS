1d6111fc43a891e28970dcc8ab1727c4a1680313
Frost Mitchell <20970221+fmitch@users.noreply.github.com>
2020-11-12 10:37:29 -0700

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-sudo apt-get install libboost-all-dev libusb-1.0.0-dev python-mako doxygen python-docutils cmake build-essential python3-pip
 git clone https://github.com/EttusResearch/uhd.git
-UHD_REPO_PATH = $PWD/uhd
+sudo apt-get install libboost-all-dev libusb-1.0.0-dev python-mako doxygen python-docutils cmake build-essential python3-pip
+UHD_REPO_PATH=$PWD/uhd
 cd $UHD_REPO_PATH/host
 mkdir build
 cd build
@@ -9,7 +9,7 @@ make -j4
 make test
 sudo make install
 sudo /usr/local/lib/uhd/utils/uhd_images_downloader.py
-sudo pip3 install pyYAML
+pip3 install pyYAML
 export LD_LIBRARY_PATH=$UHD_REPO_PATH
 export UHD_IMAGES_DIR=/usr/local/share/uhd/images
 export PYTHONPATH=$UHD_REPO_PATH/host/build/python
